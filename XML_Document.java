cb1a11b1807d0ea7845d5d8eb8193f47d2ae4ac9
samuelhild <30314228+samuelhild@users.noreply.github.com>
2017-08-02 19:19:59 -0500

    Changes to XML_Document class
    Began to move addElement() to XML_Document. added a few Setter/getter functions. added assembleDocument()

--- XML_Document.java
+++ XML_Document.java
@@ -1,6 +1,12 @@
 /**
  * Created by Samuel Hild on 7/30/2017.
  */
+
+//TODO move addElement() function to XML_Document
+//
+//
+//
+
 public class XML_Document {
     private XML_Element Elements[];
     private String x_version;
@@ -10,6 +16,38 @@ public class XML_Document {
     private final String prolog_start = "<?xml version = '";
     private final String prolog_mid = "' encoding = '";
     private final String prolog_end = "'?>\n";
+
+    //other vars
+    private boolean PrologBool = true;                              //decides whether or not to print the header to the output file
+
+    public void addElement(XML_Element parent, String... argv) {
+
+        if (Elements == null) Elements = new XML_Element[1];
+
+        if (parent == null) {                                   //When no parent is passed assume element is root
+
+        } else {
+            //parent.addChild(new XML_Element());//TODO replace addElement() with addchild in XML_Element()
+        }
+
+    }
+
+    /*public XML_Element addElement(String... argv) {
+        return addElement(null, argv);
+    }*/
+
+    private String assembleDocument() {
+        //creates  an XML document including the header and all elements
+        //returns a String xml_file
+        String xml_file = "";
+        if (PrologBool) {
+            xml_file = getProlog();                             //Check if Prolog should be outputted to the file
+        }
+
+        xml_file = xml_file + getElements();                    //add all Elements to the file
+
+        return xml_file;
+    }
 
     //Setter and Getter Functions
     //sets/gets filename
@@ -49,9 +87,12 @@ public class XML_Document {
         return Elements;
     }
 
+    public String getDocument() {
+        return assembleDocument();
+    }
+
     //Prints Entire xml Document
     public void printXmlDocument() {
-        System.out.println(prolog_start + x_version + prolog_mid + x_encoding + prolog_end);
-        for (int i = 0; i < Elements.length; i++) Elements[i].printElement();
+        System.out.println(assembleDocument());
     }
 }
